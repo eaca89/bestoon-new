4370492827f5213eec3e8c4207752bf8650447e9
Ehsan <eaca89@gamil.com>
2022-01-03 14:29:37 +0330

    income model in added

--- bestoon/web/models.py
+++ bestoon/web/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
